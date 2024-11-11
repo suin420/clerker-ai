9f8e0aa30ae7f27faa3160f889f7ae610362a2e3
chaewonkimm <kimcwbf@gmail.com>
2024-11-12 02:54:44 +0900

    feat: update sagemaker files

--- lambda_function.py
+++ lambda_function.py
@@ -3,12 +3,11 @@ import json
 import boto3
 from STT.ClovaText import make_stt_txt
 from Chunking.EmbeddingChunking import make_chunk
-from Keywords.BllossomKeyword_to_md import generate_summary_jsons, generate_report_from_json
-from Diagrams.DiagramGeneration import diagram_gen
 
 ACCESS_KEY = os.environ.get("ACCESS_KEY")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 bucket_name = "clerkerai"
+region_name = "eu-north-1"
 
 s3 = boto3.client(
     's3',
@@ -42,7 +41,6 @@ def download_folder_from_s3(s3_folder, local_dir):
 
 def lambda_handler(event, context):
     os.environ['MPLCONFIGDIR'] = '/tmp/matplotlib'
-    #os.environ['TRANSFORMERS_CACHE'] = '/tmp/huggingface'
     os.environ["TRANSFORMERS_CACHE"] = "/tmp/.cache/huggingface"
     os.environ['NUMBA_CACHE_DIR'] = '/tmp/numba_cache'
 
@@ -50,32 +48,18 @@ def lambda_handler(event, context):
     os.makedirs('/tmp/STT/stt_audio', exist_ok=True)
     os.makedirs('/tmp/STT/stt_text/KeywordBoosting', exist_ok=True)
     os.makedirs('/tmp/Chunking', exist_ok=True)
-    os.makedirs('/tmp/Keywords', exist_ok=True)
-    os.makedirs('/tmp/Diagrams', exist_ok=True)
-    os.makedirs('/tmp/Diagrams/mermaid', exist_ok=True)
-    os.makedirs('/tmp/models', exist_ok=True)
     os.makedirs('/tmp/models/models--jhgan--ko-sroberta-sts/snapshots/3efa8e54a06798b00bd1abb9c22b2dd530e22b24/', exist_ok=True)
-    os.makedirs('/tmp/models/models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M/snapshots/4e602ad115392e7298674e092d6f8b45138f1db7/', exist_ok=True)
-    os.makedirs('/tmp/Keywords/NanumFontSetup_TTF_SQUARE_ROUND', exist_ok=True)
 
     os.makedirs("/tmp/.cache/huggingface", exist_ok=True)
-    #os.makedirs('/tmp/huggingface', exist_ok=True)
     os.makedirs('/tmp/matplotlib', exist_ok=True)
     os.makedirs('/tmp/numba_cache', exist_ok=True)
-    print("current directory: ", os.getcwd())
 
     input_domain = event.get('domain', 'IT')
     mp3_file_url = event.get('mp3FileUrl', "input_audio.mp3")
-    print("mp3_file_url: ", mp3_file_url)
 
     input_audio_file = '/tmp/STT/stt_audio/input_audio.mp3'
     output_txt_file = '/tmp/STT/stt_text/stt_text.txt'
     output_chunk_dict = '/tmp/Chunking/chunking_text.json'
-    output_summary_json = '/tmp/Keywords/summary.json'
-    diagram_summary_json = '/tmp/Diagrams/diagram_summary.json'
-    output_report_md = '/tmp/Keywords/report.md'
-    s3_font_path = 'Keywords/NanumFontSetup_TTF_SQUARE_ROUND/NanumSquareRoundB.ttf'
-    font_path = f'/tmp/{s3_font_path}'
 
     download_from_s3(mp3_file_url, input_audio_file)
 
@@ -90,17 +74,13 @@ def lambda_handler(event, context):
     local_model_dir = '/tmp/models'
     os.makedirs(local_model_dir, exist_ok=True)
 
-    model_folders = [
-        'models--jhgan--ko-sroberta-sts/',
-        'models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M/snapshots/4e602ad115392e7298674e092d6f8b45138f1db7/',
-    ]
-    for model_folder in model_folders:
-        s3_model_folder = f'models/{model_folder}'
-        local_model_folder = os.path.join(local_model_dir, model_folder)
+    model_folder = 'models/models--jhgan--ko-sroberta-sts/'
+
+    for file in model_folder:
+        s3_model_folder = f'models/{file}'
+        local_model_folder = os.path.join(local_model_dir, file)
         print(f"Downloading model folder from S3: {s3_model_folder}")
         download_folder_from_s3(s3_model_folder, local_model_folder)
-
-    download_from_s3(s3_font_path, font_path)
 
     make_stt_txt(
         input_domain,
@@ -112,37 +92,85 @@ def lambda_handler(event, context):
     make_chunk(output_txt_file, output_chunk_dict)
     print(f"Chunk Dict 파일 생성 완료 : {output_chunk_dict}")
 
-    generate_summary_jsons(
-        output_chunk_dict,
-        diagram_summary_json,
-        output_summary_json,
-        model_id=os.path.join(local_model_dir, 'models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M/snapshots/4e602ad115392e7298674e092d6f8b45138f1db7/'),
-        model_path=os.path.join(local_model_dir, 'models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M/snapshots/4e602ad115392e7298674e092d6f8b45138f1db7/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf')
-    )
-    print(f"다이어그램 및 보고서용 JSON 파일 생성 완료 : {output_summary_json}")
-
-    diagram_gen(diagram_summary_json)
-    print("다이어그램 생성 완료")
-
-    generate_report_from_json(output_summary_json, output_report_md)
-    print(f"Report 파일 생성 완료 : {output_report_md}")
-
     upload_to_s3(output_txt_file, 'STT/stt_text/stt_text.txt')
     upload_to_s3(output_chunk_dict, 'Chunking/chunking_text.json')
-    upload_to_s3(output_summary_json, 'Keywords/summary.json')
-    upload_to_s3(output_report_md, 'Keywords/report.md')
 
-    diagram_images_dir = '/tmp/Diagrams/mermaid'
-    for root, dirs, files in os.walk(diagram_images_dir):
-        for file in files:
-            local_file_path = os.path.join(root, file)
-            s3_file_path = f'Diagrams/mermaid/{file}'
-            upload_to_s3(local_file_path, s3_file_path)
+    sagemaker_client = boto3.client('sagemaker', region_name='eu-north-1')
+
+    processing_job_name = f"generate-summary-{context.aws_request_id}"
+
+    response = sagemaker_client.start_processing_job(
+        ProcessingJobName=processing_job_name,
+        ProcessingInputs=[
+            {
+                'InputName': 'input-data',
+                'S3Input': {
+                    'S3Uri': f's3://clerkerai/Chunking/chunking_text.json',
+                    'LocalPath': '/opt/ml/processing/input',
+                    'S3DataType': 'S3Prefix',
+                    'S3InputMode': 'File'
+                }
+            },
+
+            {
+                'InputName': 'model-data',
+                'S3Input': {
+                    'S3Uri': f's3://clerkerai/models/models--MLP-KTLim--llama-3-Korean-Bllossom-8B-gguf-Q4_K_M/snapshots/4e602ad115392e7298674e092d6f8b45138f1db7/',
+                    'LocalPath': '/opt/ml/processing/models',
+                    'S3DataType': 'S3Prefix',
+                    'S3InputMode': 'File'
+                }
+            },
+
+            {
+                'InputName': 'font-data',
+                'S3Input': {
+                    'S3Uri': f's3://clerkerai/Keywords/NanumFontSetup_TTF_SQUARE_ROUND/NanumSquareRoundB.ttf',
+                    'LocalPath': '/opt/ml/processing/fonts',
+                    'S3DataType': 'S3Object',
+                    'S3InputMode': 'File'
+                }
+            },
+        ],
+        ProcessingOutputConfig={
+            'Outputs': [
+                {
+                    'OutputName': 'output-data',
+                    'S3Output': {
+                        'S3Uri': f's3://clerkerai/ProcessingOutput/',
+                        'LocalPath': '/opt/ml/processing/output',
+                        'S3UploadMode': 'EndOfJob'
+                    }
+                },
+            ]
+        },
+        ProcessingResources={
+            'ClusterConfig': {
+                'InstanceCount': 1,
+                'InstanceType': 'ml.t3.medium', #ml.g4dn.xlarge
+                'VolumeSizeInGB': 10
+            }
+        },
+        AppSpecification={
+            'ImageUri': '715841353635.dkr.ecr.eu-north-1.amazonaws.com/clerker-ai:latest',
+            'ContainerEntrypoint': ['python3', 'sagemaker_entrypoint.py'],
+            'ContainerArguments': [
+                '--input_chunk_dict', '/opt/ml/processing/input/chunking_text.json',
+                '--diagram_summary_json', '/opt/ml/processing/output/diagram_summary.json',
+                '--report_summary_json', '/opt/ml/processing/output/summary.json',
+                '--model_id', '/opt/ml/processing/models/',
+                '--model_path', '/opt/ml/processing/models/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf',
+                '--font_path', '/opt/ml/processing/fonts/NanumSquareRoundB.ttf'
+            ]
+        },
+        RoleArn='arn:aws:iam::your-account-id:role/your-sagemaker-execution-role'
+    )
+
+    print(f"SageMaker Processing Job '{processing_job_name}'이 시작되었습니다.")
 
     response = {
-        "report": f"https://{bucket_name}.s3.amazonaws.com/Keywords/report.md",
-        "stt": f"https://{bucket_name}.s3.amazonaws.com/STT/stt_text/stt_text.txt",
-        "diagram_image": f"https://{bucket_name}.s3.amazonaws.com/Diagrams/mermaid.zip"
+        "message": f"SageMaker Processing Job '{processing_job_name}' started.",
+        "processing_job_name": processing_job_name
     }
 
     return {
